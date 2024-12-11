8e41042f1944571cca4f51b4476e11b230166a8d
freyes <fran@codesai.com>
2024-12-11 19:42:05 +0000

    create an example

--- gilded-rose/Gilded-rose.Test/GildedRoseTest.cs
+++ gilded-rose/Gilded-rose.Test/GildedRoseTest.cs
@@ -6,9 +6,16 @@ namespace Gilded_rose.Test
     public class GildedRoseTest
     {
         [Test]
-        public void Canary_Test()
+        public void Regular_Item_Decrease_Quality_By_One()
         {
-            Assert.That(true, Is.True);
+            var item = new Item("Regular", 10, 20);
+            var gildedRose = new GildedRose( new List<Item> { item });
+            
+            gildedRose.UpdateQuality();
+            
+            Assert.That(item.Name, Is.EqualTo("Regular"));
+            Assert.That(item.SellIn, Is.EqualTo(9));
+            Assert.That(item.Quality, Is.EqualTo(19));
         }
 
     }
