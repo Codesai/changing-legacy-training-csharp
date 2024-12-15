548f94c502dfb3da64ef03a1d63ca93cdc53df27
freyes <fran@codesai.com>
2024-12-15 23:27:29 +0000

    fix error in report. The differences were not being shown. Candidate value was the value control.

--- scientist/Calculator/scientist/MortgagePaymentsResultPublisher.cs
+++ scientist/Calculator/scientist/MortgagePaymentsResultPublisher.cs
@@ -16,20 +16,20 @@ public class MortgagePaymentsResultPublisher : IResultPublisher
     {
         var report = $"Results for experiment '{result.ExperimentName}' on {DateTime.Now}\n";
         report += $"Result: {(result.Matched ? "MATCH" : "MISMATCH")}\n";
-        report += $"Control value: {ConvertToMortgageData(result)}\n";
+        report += $"Control value: {ConvertToMortgageData(result.Control.Value)}\n";
         report +=$"Control duration: {result.Control.Duration}\n";
         foreach (var observation in result.Candidates)
         {
-            report +=$"Candidate value: {ConvertToMortgageData(result)}\n";
+            report +=$"Candidate value: {ConvertToMortgageData(observation.Value)}\n";
             report +=$"Candidate duration: {observation.Duration}\n";
         }
         report += "== End experiment ==\n\n";
         return File.AppendAllTextAsync(_resultFilePath, report);
     }
 
-    private string ConvertToMortgageData<T, TClean>(Result<T, TClean> result)
+    private string ConvertToMortgageData<T>(T value)
     {
-        var data = result.Control.Value as List<MortgagePayment>;
+        var data = value as List<MortgagePayment>;
         return string.Join("\n", data);
     }
 }
