1b93fdfdd3615e4049c260352808df51cc079313
trikitrok <trikitrok@gmail.com>
2025-01-16 20:40:54 +0100

    starting with a different test and giving some factory methods

--- legacy-argent-rose-broken-dependencies/LegacyArgentRose.Tests/ArgentRoseStoreTest.cs
+++ legacy-argent-rose-broken-dependencies/LegacyArgentRose.Tests/ArgentRoseStoreTest.cs
@@ -1,19 +1,30 @@
 using NUnit.Framework;
+using static ArgentRose.Tests.ArgentRoseStoreForTesting;
 
 namespace ArgentRose.Tests;
 
 public class ArgentRoseStoreTest
 {
     [Test]
-    public void Regular_Product_Decreases_Quality_By_Two()
+    public void Expired_Theatre_Passes_Drop_Quality_To_Zero()
     {
-        var initialInventory = new List<Product> { new("RegularProduct", 1, 10) };
-        var store = new ArgentRoseStoreForTesting(initialInventory);
+        var store = StoreIncluding(TheatrePasses(0, 5));
 
         store.Update();
 
-        var expectedInventory = new List<Product> { new("RegularProduct", 0, 8) };
-        Assert.That(store.SavedInventory, Is.EqualTo(expectedInventory));
+        Assert.That(
+            store.SavedInventory,
+            Is.EqualTo(InventoryIncluding(TheatrePasses(-1, 0))));
+    }
+
+    private static Product TheatrePasses(int sellIn, int quality)
+    {
+        return new Product("Theatre Passes", sellIn, quality);
+    }
+
+    private static List<Product> InventoryIncluding(params Product[] products)
+    {
+        return products.ToList();
     }
 }
 
@@ -22,7 +33,14 @@ public class ArgentRoseStoreForTesting : ArgentRoseStore
     private List<Product> _savedInventory;
     private readonly List<Product> _initialInventory;
 
-    public ArgentRoseStoreForTesting(List<Product> initialInventory)
+    public static ArgentRoseStoreForTesting StoreIncluding(params Product[] products)
+    {
+        return new ArgentRoseStoreForTesting(products.ToList());
+    }
+    
+    public List<Product> SavedInventory => _savedInventory;
+
+    private ArgentRoseStoreForTesting(List<Product> initialInventory)
     {
         _savedInventory = new List<Product>();
         _initialInventory = initialInventory;
@@ -37,6 +55,4 @@ public class ArgentRoseStoreForTesting : ArgentRoseStore
     {
         _savedInventory = inventory;
     }
-
-    public List<Product> SavedInventory => _savedInventory;
 }
